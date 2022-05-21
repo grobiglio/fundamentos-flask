e064662353617a1a8e91d5828be428307afcff72
Guillermo A. Robiglio <grobiglio@gmail.com>
2022-05-21 16:52:11 -0300

    Hasta el minuto 43

--- app/app.py
+++ app/app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Conexión MySQL
+app.config['MYSQL_HOST'] = 'localhost:3306'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'abaco'
+
+conexion = MySQL(app)
+
+@app.before_request
+def before_request():
+    print('Antes de la petición ...')
+
+
+@app.after_request
+def after_request(response):
+    print('Después de la petición.')
+    return response
 
 
 @app.route('/')
@@ -16,5 +35,30 @@ def index():
     return render_template('index.html', data=data)
 
 
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre, edad):
+    data = {
+        'titulo': 'Contacto',
+        'nombre': nombre,
+        'edad': edad
+    }
+    return render_template('contacto.html', data=data)
+
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "Ok"
+
+
+def pagina_no_encontrada(error):
+    # return render_template('404.html'), 404
+    return redirect(url_for('index'))
+    
+
 if __name__ == '__main__':
+    app.add_url_rule('/query_string', view_func=query_string)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=5000)
