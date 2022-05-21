ee3d537d3b347e7d237bce7649850fe5fe310333
Guillermo A. Robiglio <grobiglio@gmail.com>
2022-05-21 02:27:06 -0300

    Hasta el minuto 20

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # return "<h1>Hola Guillermo!</h1>"
+    cursos = ['PHP', 'Python', 'HTML', 'CSS', 'Javascript', 'SQL']
+    data = {
+        'titulo': 'Index 123',
+        'bienvenida': '¡Saludos!',
+        'cursos': cursos,
+        'nro_cursos': len(cursos)
+    }
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
